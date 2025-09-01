b9b028b411152c5a4c294cf73c4ae87828352e8d
rahul <rahulkandolkar@gmail.com>
2025-09-01 11:14:25 +0530

    Implement HighArray class with insert, findElement, delete, and display methods

---
+++ src/arrays/HighArray.java
@@ -0,0 +1,48 @@
+package arrays;
+
+public class HighArray {
+    private int[] array;
+    private int nElement;
+
+    public HighArray(int maxSize){
+        this.array = new int[maxSize];
+        nElement = 0;
+    }
+
+    public int findElement(int value){
+        for(int i = 0; i < nElement; i++){
+            if(array[i] == value){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void insert(int value){
+        array[nElement] = value;
+        nElement += 1;
+    }
+
+    public boolean delete(int value){
+        int index = findElement(value);
+        if(index != -1){
+            for(int i = index; i < nElement - 1; i++){
+                array[i] = array[i + 1];
+            }
+            nElement -= 1;
+            return true;
+        }
+        return false;
+    }
+
+    public void display(){
+        for(int i = 0; i < nElement; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+
+
+}
---
+++ src/arrays/Main.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int maxSize = 100;
+        HighArray array = new HighArray(maxSize);
+
+        array.insert(77);
+        array.insert(99);
+        array.insert(44);
+        array.insert(55);
+        array.insert(22);
+        array.insert(88);
+        array.insert(11);
+        array.insert(00);
+        array.insert(66);
+        array.insert(33);
+
+        array.display();
+
+        array.findElement(44);
+    }
+}
